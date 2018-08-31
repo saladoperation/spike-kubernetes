0d39de55bf66f87ee22a70bd71fc8d89f943627a
Your Name <you@example.com>
2018-08-31 11:51:42 +0000

    Add index.html

--- project.clj
+++ project.clj
@@ -2,8 +2,12 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [fogus/ring-edn "0.3.0"]
+                 [mount "0.1.12"]
+                 [org.immutant/immutant "2.1.10"]
+                 [ring "1.6.3"]]
   :plugins [[lein-ancient "0.6.15"]]
   :main ^:skip-aot spike-kubernetes.core
   :target-path "target/%s"
---
+++ src/spike_kubernetes/serve.clj
@@ -0,0 +1,18 @@
+(ns spike-kubernetes.serve
+  (:require [immutant.web :as web]
+            [mount.core :as mount :refer [defstate]]
+            [ring.middleware.edn :refer [wrap-edn-params]]
+            [ring.middleware.resource :refer [wrap-resource]]))
+
+(def handler)
+
+(def start
+  (partial web/run
+           (-> handler
+               (wrap-resource "public")
+               wrap-edn-params)
+           {:host "0.0.0.0"}))
+
+(defstate server
+          :start (start)
+          :stop (web/stop))
