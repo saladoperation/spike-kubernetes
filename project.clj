244c70ad75d940bcc0aa12c53683b2cb90f3ec10
Your Name <you@example.com>
2018-09-02 13:28:48 +0000

    Use lein-cljsbuild

--- project.clj
+++ project.clj
@@ -7,13 +7,20 @@
                  [org.clojure/clojurescript "1.10.339"]
                  [fogus/ring-edn "0.3.0"]
                  [hiccup "1.0.5"]
-                 [mount "0.1.12"]
+                 [mount "0.1.13"]
                  [org.immutant/immutant "2.1.10"]
                  [reagent "0.8.1"]
                  [ring "1.6.3"]]
-  :plugins [[lein-ancient "0.6.15"]]
+  :plugins [[lein-ancient "0.6.15"]
+            [lein-cljsbuild "1.1.5"]]
   :main ^:skip-aot spike-kubernetes.core
   :target-path "target/%s"
+  :cljsbuild {:builds
+              {:prod
+               {:source-paths ["src"]
+                :compiler     {:output-to     "resources/public/js/main.js"
+                               :main          spike_kubernetes.web
+                               :optimizations :advanced}}}}
   :profiles {:dev     {:dependencies [[binaryage/devtools "0.9.10"]
                                       [figwheel-sidecar "0.5.14"]]}
              :uberjar {:uberjar-name "main.jar"}})
