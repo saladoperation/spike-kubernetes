ffe1ff2070c504e220dbb648eb3c3a5398af3526
Your Name <you@example.com>
2018-09-04 07:43:36 +0000

    Move commands to a script

---
+++ script/gcloud.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+gcloud auth activate-service-account --key-file=<(echo $GCLOUD_KEY) &&
+gcloud -q config set project $GCLOUD_PROJECT &&
+gcloud -q config set compute/zone $GCLOUD_COMPUTE_ZONE &&
+gcloud container clusters get-credentials cluster
+kubectl apply -f kubernetes.txt
