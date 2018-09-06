d06677921dc3955c217a0d6ae009e3988fc35c60
Your Name <you@example.com>
2018-09-06 12:30:09 +0000

    Run lein doo on CircleCI

--- src/spike_kubernetes/circleci.clj
+++ src/spike_kubernetes/circleci.clj
@@ -27,9 +27,7 @@
 (defn get-dockerfile
   [{:keys [image from-tos run port cmd] :or {run ":"}}]
   (generate-dockerfile (concat [["FROM" image]]
-                               (map (partial s/setval*
-                                             s/BEFORE-ELEM
-                                             "COPY")
+                               (map (partial s/setval* s/BEFORE-ELEM "COPY")
                                     from-tos)
                                [["WORKDIR" (get-code-path)]
                                 ["RUN" ["/bin/bash" "-c" run]]
@@ -45,13 +43,16 @@
 (def uberjar
   "uberjar")
 
+(def java-image
+  (str
+    java
+    ":8u111-jdk@sha256:c1ff613e8ba25833d2e1940da0940c3824f03f802c449f3d1815a66b7f8c0e9d"))
+
 (def clojure-dockerfile
-  (get-dockerfile
-    {:image    (str java
-                    ":8u111-jdk@sha256:c1ff613e8ba25833d2e1940da0940c3824f03f802c449f3d1815a66b7f8c0e9d")
-     :from-tos #{[(get-target-path uberjar jar) (get-code-path jar)]}
-     :port     helpers/clojure-port
-     :cmd      [java "-jar" jar "serve"]}))
+  (get-dockerfile {:image    java-image
+                   :from-tos #{[(get-target-path uberjar jar) (get-code-path jar)]}
+                   :port     helpers/clojure-port
+                   :cmd      [java "-jar" jar "serve"]}))
 
 (def get-from-tos
   (partial map (comp (partial s/transform* s/LAST get-code-path)
@@ -66,10 +67,14 @@
 (def node
   "node")
 
+(def node-image
+  (str
+    node
+    ":8.11.4@sha256:fd3c42d91fcf6019eec4e6ccd38168628dd4660992a1550a71c7a7e2b0dc2bdd"))
+
 (def clojurescript-dockerfile
   (get-dockerfile
-    {:image    (str node
-                    ":8.11.4@sha256:fd3c42d91fcf6019eec4e6ccd38168628dd4660992a1550a71c7a7e2b0dc2bdd")
+    {:image    node-image
      :from-tos (get-from-tos #{(get-prod-path) node-modules})
      :port     helpers/clojurescript-port
      :cmd      [node (get-prod-path "main.js")]}))
@@ -80,18 +85,23 @@
 (def script
   "script")
 
+(def conda-image
+  "continuumio/miniconda:4.5.4@sha256:19d3eedab8b6301a0e1819476cfc50d53399881612183cf65208d7d43db99cd9")
+
 (defn get-python-dockerfile
   [entrypoint]
   (get-dockerfile
-    {:image    "continuumio/miniconda:4.5.4@sha256:19d3eedab8b6301a0e1819476cfc50d53399881612183cf65208d7d43db99cd9"
+    {:image    conda-image
      :from-tos (get-from-tos #{python script})
-     :run      (str/join " && " (map (partial str/join " ") [["conda"
-                                                              "env"
-                                                              "create"
-                                                              "-f"
-                                                              (helpers/join-paths python "environments/cpu.yml")]
-                                                             ["source" "activate" "spike-kubernetes"]
-                                                             [python "-m" "spacy" "download" "en"]]))
+     :run      (->> [["conda"
+                      "env"
+                      "create"
+                      "-f"
+                      (helpers/join-paths python "environments/cpu.yml")]
+                     ["source" "activate" "spike-kubernetes"]
+                     [python "-m" "spacy" "download" "en"]]
+                    (map (partial str/join " "))
+                    (str/join " && "))
      :port     8000
      :cmd      [(helpers/join-paths script "flask" entrypoint "prod.sh")]}))
 
@@ -167,20 +177,24 @@
         (s/transform s/MAP-KEYS get-dockerfile-path)
         (run! (partial apply spit+))))
 
+(def run-tests
+  #(->> [["test"]
+         ["doo" node "test" "once"]]
+        (map (partial apply command/lein))
+        (apply m/>>)))
+
 (defn run-circleci
   []
   (spit-dockerfiles+)
   (kubernetes/spit-kubernetes)
   (effect println
-          (m/>>= (m/>> (command/docker "login"
-                                       "-u"
-                                       helpers/username
-                                       "-p"
-                                       (:docker-password env))
+          (m/>>= (m/>> (->> env
+                            :docker-password
+                            (command/docker "login" "-u" helpers/username "-p"))
                        (build-clojure)
                        (build-clojurescript)
                        (build-docker helpers/parse-name)
-                       (command/lein "test"))
+                       (run-tests))
                  #(aid/casep env
                              :circle-tag (->> helpers/port
                                               keys
