bf915f316a413c272cfd2a6db55ffaf3b71221e0
Your Name <you@example.com>
2018-09-08 00:55:29 +0000

    Don't source pretrained.sh

--- script/python/parse/prod.sh
+++ script/python/parse/prod.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-source script/python/parse/pretrained.sh
 source script/python/helpers.sh
 #with export FLASK_DEBUG=1, Flask seems to use almost twice as much memory as without it
 gunicorn -t 3000 spike_kubernetes.parse:app
