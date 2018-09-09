87578ea4c3ad708e4be4666cc9a1f1fb29a0b273
Your Name <you@example.com>
2018-09-09 23:25:05 +0000

    Specify the port

--- script/python/document/prod.sh
+++ script/python/document/prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source script/python/helpers.sh
 #with export FLASK_DEBUG=1, Flask seems to use almost twice as much memory as without it
-gunicorn -t 3000 spike_kubernetes.document.evaluate:app
+gunicorn -b localhost:8001 -t 3000 spike_kubernetes.document.evaluate:app
