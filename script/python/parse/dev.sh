0ba3ad716176514f2b6147ba072ce69ad5b59b4f
Your Name <you@example.com>
2018-09-09 01:09:22 +0000

    Specify parse.py

--- script/python/parse/dev.sh
+++ script/python/parse/dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source script/python/helpers.sh
-export FLASK_APP=spike_sentence/core.py FLASK_DEBUG=1
+export FLASK_APP=spike_kubernetes/parse.py FLASK_DEBUG=1
 flask run -p 8000
