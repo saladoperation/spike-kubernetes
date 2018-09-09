19f37272fd73c052556d1b1da432a69afd29d557
Your Name <you@example.com>
2018-09-09 22:11:07 +0000

    Delete newlines

--- python/spike_kubernetes/helpers.py
+++ python/spike_kubernetes/helpers.py
@@ -4,9 +4,8 @@ from spike_kubernetes.clojure.core import *
 
 def make_index_(m):
     def index_():
-        return jsonify(
-            apply(
-                m[request.get_json()["action"]],
-                ((request.get_json()["data"]),) if contains_(request.get_json(),
-                                                             "data") else ()))
+        return jsonify(apply(m[request.get_json()["action"]],
+                             ((request.get_json()["data"]),
+                              ) if contains_(request.get_json(),
+                                             "data") else ()))
     return index_
