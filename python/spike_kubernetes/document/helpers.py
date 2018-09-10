843b76f7c0292f40185339da1acf72e97a03c737
Your Name <you@example.com>
2018-09-10 05:42:54 +0000

    Implement get-stoi

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -1,3 +1,7 @@
+import torchtext.vocab as vocab
+from spike_kubernetes.clojure.core import *
 import spike_kubernetes.helpers as helpers
 
-index_ = helpers.make_index_({})
+pretrained = vocab.Vectors("deps.words")
+get_stoi = constantly(pretrained.stoi)
+index_ = helpers.make_index_({"get-stoi": get_stoi})
