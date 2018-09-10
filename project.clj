847f443dec081a236468b155236df4279c488736
Your Name <you@example.com>
2018-09-10 09:14:42 +0000

    Update figwheel-sidecar

--- project.clj
+++ project.clj
@@ -30,7 +30,7 @@
   :profiles {:dev  {:dependencies [[binaryage/devtools "0.9.10"]
                                    [buddy "2.0.0"]
                                    [environ "1.1.0"]
-                                   [figwheel-sidecar "0.5.14"]]}
+                                   [figwheel-sidecar "0.5.16"]]}
              :prod {}}
   :cljsbuild
   {:builds
