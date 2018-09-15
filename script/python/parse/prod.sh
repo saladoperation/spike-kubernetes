baf3984d74387a415167b1b54cc8c331ec5bad94
Your Name <you@example.com>
2018-09-15 13:38:01 +0000

    Use 0.0.0.0

--- script/python/parse/prod.sh
+++ script/python/parse/prod.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source script/python/helpers.sh
 #with export FLASK_DEBUG=1, Flask seems to use almost twice as much memory as without it
-gunicorn spike_kubernetes.parse:app
+gunicorn -b 0.0.0.0:8000 spike_kubernetes.parse:app
