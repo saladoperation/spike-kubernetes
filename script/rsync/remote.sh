1be088920643e10a4692a7c3c56a4874e5394470
Your Name <you@example.com>
2018-09-19 07:43:18 +0000

    Add remote.sh

---
+++ script/rsync/remote.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rsync_selectively(){
+    rsync -azP --exclude=.idea --filter=':- /python/.gitignore' $(pwd) $1
+};
+rsync_selectively $1
+while inotifywait -e create -r .git; do
+    #.gitignore in the project root seems to be used as a filter by default
+    rsync_selectively $1
+done
