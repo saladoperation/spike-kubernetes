896de947547d1e277c5f85ba9eb03cd9c13e4823
Your Name <you@example.com>
2018-09-20 02:42:29 +0000

    Use an environmental variable for a cluster name

--- script/gcloud.sh
+++ script/gcloud.sh
@@ -2,5 +2,5 @@
 gcloud auth activate-service-account --key-file=<(echo $GCLOUD_KEY) &&
 gcloud -q config set project $GCLOUD_PROJECT &&
 gcloud -q config set compute/zone $GCLOUD_COMPUTE_ZONE &&
-gcloud container clusters get-credentials cluster
+gcloud container clusters get-credentials $GCLOUD_CLUSTER &&
 kubectl apply -f kubernetes.txt
