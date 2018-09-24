027522eb7633e35d79893f557c5dbe1b3d4365a2
Your Name <you@example.com>
2018-09-24 07:49:16 +0000

    Implement get_embedding_vectors

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -1,6 +1,11 @@
+import torch
 import torchtext.vocab as vocab
 from spike_kubernetes.clojure.core import *
 import spike_kubernetes.helpers as helpers
 
 pretrained = vocab.Vectors("deps.words")
 index_ = helpers.make_index_({"get-stoi": constantly(pretrained.stoi)})
+
+
+def get_embedding_vectors(unk_vector):
+    return torch.cat((pretrained.vectors, unk_vector))
