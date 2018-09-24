b8558538be9fec22ad02f0785883cb4df75edcab
Your Name <you@example.com>
2018-09-24 08:51:42 +0000

    Decode a payload

--- python/spike_kubernetes/document/training.py
+++ python/spike_kubernetes/document/training.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import pika
 from spike_kubernetes.clojure.core import *
+import spike_kubernetes.aid as aid
 import spike_kubernetes.document.helpers as document_helpers
 
 app = Flask(__name__)
@@ -14,6 +15,18 @@ def index():
 channel = pika.BlockingConnection().channel()
 queue = "queue"
 channel.queue_declare(queue)
+
+
+def make_attribute_call(s_):
+    return comp(aid.build(partial(apply, aid.funcall),
+                          comp(partial(aid.flip(getattr), s_),
+                               first),
+                          rest),
+                vector)
+
+
 # TODO implement this function
-get_steps = comp(partial(map, last))
+get_steps = comp(partial(map, comp(make_attribute_call("decode"))),
+                 partial(remove, partial(equal, None)),
+                 partial(map, last))
 steps = get_steps(repeatedly(partial(channel.basic_get, queue, True)))
