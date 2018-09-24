018365454dba81798f3a25a182eb0c784d38cfc4
Your Name <you@example.com>
2018-09-24 17:52:14 +0000

    Call ModuleDict

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -29,23 +29,23 @@ num_tags = multiply(3, 2)
 
 
 def get_model():
-    model = nn.Module()
     # TODO possibly use embedding dropout
-    model.embedding = get_embedding(
-        init.kaiming_normal_(torch.zeros(1,
-                                         pretrained.dim)))
-    # TODO possibly use variational dropout
-    # TODO possibly use layer normalization
-    model.lstm = nn.LSTM(pretrained.dim,
-                         tuned["hidden_size"],
-                         tuned["num_layers"],
-                         batch_first=True,
-                         bidirectional=True,
-                         dropout=tuned["dropout"])
-    model.linear = nn.Linear(
-        get_bidirectional_size(tuned["hidden_size"]),
-        num_tags)
-    model.crf = conditional_random_field.ConditionalRandomField(num_tags)
+    model = nn.ModuleDict(
+        {"embedding": get_embedding(
+            init.kaiming_normal_(torch.zeros(1,
+                                             pretrained.dim))),
+            # TODO possibly use variational dropout
+            # TODO possibly use layer normalization
+            "lstm": nn.LSTM(pretrained.dim,
+                            tuned["hidden_size"],
+                            tuned["num_layers"],
+                            batch_first=True,
+                            bidirectional=True,
+                            dropout=tuned["dropout"]),
+            "linear": nn.Linear(
+                get_bidirectional_size(tuned["hidden_size"]),
+                num_tags),
+            "crf": conditional_random_field.ConditionalRandomField(num_tags)})
     return model
 
 
