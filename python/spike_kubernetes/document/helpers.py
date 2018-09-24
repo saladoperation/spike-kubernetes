0b10363430b6fb096bc28cbc01ad9117afd72b38
Your Name <you@example.com>
2018-09-24 17:32:36 +0000

    Define get_model

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.init as init
 import torchtext.vocab as vocab
 from spike_kubernetes.clojure.core import *
 from spike_kubernetes.cheshire import *
@@ -20,5 +21,25 @@ def get_embedding_vectors(unk_vector):
 
 get_embedding = comp(partial(aid.flip(nn.Embedding.from_pretrained), False),
                      get_embedding_vectors)
+
+
+def get_model():
+    model = nn.Module()
+    # TODO possibly use embedding dropout
+    model.embedding = get_embedding(
+        init.kaiming_normal_(torch.zeros(1,
+                                         pretrained.dim)))
+    # TODO possibly use variational dropout
+    # TODO possibly use layer normalization
+    model.lstm = nn.LSTM(pretrained.dim,
+                         tuned["hidden_size"],
+                         tuned["num_layers"],
+                         batch_first=True,
+                         bidirectional=True,
+                         dropout=tuned["dropout"])
+    # TODO implement this function
+    return model
+
+
 index_ = helpers.make_index_(
     {helpers.get_stoi_name: constantly(pretrained.stoi)})
