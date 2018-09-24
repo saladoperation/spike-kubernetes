eb67b432812cce1be4ed262d719d9c93b35c34cc
Your Name <you@example.com>
2018-09-24 08:43:37 +0000

    Define steps

--- python/spike_kubernetes/document/training.py
+++ python/spike_kubernetes/document/training.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import pika
+from spike_kubernetes.clojure.core import *
 import spike_kubernetes.document.helpers as document_helpers
 
 app = Flask(__name__)
@@ -13,3 +14,6 @@ def index():
 channel = pika.BlockingConnection().channel()
 queue = "queue"
 channel.queue_declare(queue)
+# TODO implement this function
+get_steps = comp(partial(map, last))
+steps = get_steps(repeatedly(partial(channel.basic_get, queue, True)))
