e26071ba11cc79e354be40da6d2b635cf2116a62
Your Name <you@example.com>
2018-09-24 17:30:29 +0000

    Implement get_embedding

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -1,8 +1,10 @@
 import torch
+import torch.nn as nn
 import torchtext.vocab as vocab
 from spike_kubernetes.clojure.core import *
 from spike_kubernetes.cheshire import *
 import spike_kubernetes.helpers as helpers
+import spike_kubernetes.aid as aid
 
 pretrained = vocab.Vectors("deps.words")
 document_name = "document"
@@ -11,9 +13,12 @@ selection = parse_string(slurp(selection_path))
 tuned_path = helpers.get_tuned_path(document_name, selection["run"])
 tuned = parse_string(slurp(tuned_path))
 
+
 def get_embedding_vectors(unk_vector):
     return torch.cat((pretrained.vectors, unk_vector))
 
 
+get_embedding = comp(partial(aid.flip(nn.Embedding.from_pretrained), False),
+                     get_embedding_vectors)
 index_ = helpers.make_index_(
     {helpers.get_stoi_name: constantly(pretrained.stoi)})
