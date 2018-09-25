8145732dfc8fb394af30e53a2cd99962f42c11b8
Your Name <you@example.com>
2018-09-25 02:58:11 +0000

    Call get_tuned

--- python/spike_kubernetes/document/helpers.py
+++ python/spike_kubernetes/document/helpers.py
@@ -4,16 +4,13 @@ import torch.nn as nn
 import torch.nn.init as init
 import torchtext.vocab as vocab
 from spike_kubernetes.clojure.core import *
-from spike_kubernetes.cheshire import *
 import spike_kubernetes.helpers as helpers
 import spike_kubernetes.aid as aid
 
 pretrained = vocab.Vectors("deps.words")
 document_name = "document"
-selection_path = helpers.get_selection_path(document_name)
-selection = parse_string(slurp(selection_path))
-tuned_path = helpers.get_tuned_path(document_name, selection["run"])
-tuned = parse_string(slurp(tuned_path))
+selection = helpers.get_selection(document_name)
+tuned = helpers.get_tuned(document_name)
 
 
 def get_embedding_vectors(unk_vector):
