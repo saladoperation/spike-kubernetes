08f89ea531f77c0cb79820427567fa865ac6b445
Your Name <you@example.com>
2018-09-28 16:33:07 +0000

    Delete delete newlines

--- src/orchestration/spike_kubernetes/install.clj
+++ src/orchestration/spike_kubernetes/install.clj
@@ -7,11 +7,7 @@
   (partial command/sudo "apt-get"))
 
 (def apt-packages
-  #{"inotify-tools"
-    "nodejs"
-    "silversearcher-ag"
-    "tmux"
-    "vim"})
+  #{"inotify-tools" "nodejs" "silversearcher-ag" "tmux" "vim"})
 
 (def install-apt
   #(m/>> (apt-get "update")
