6b80adb03a08da7245642072405f16445725ce09
Your Name <you@example.com>
2018-09-29 07:51:20 +0000

    Use line-seq in organize

--- src/orchestration/spike_kubernetes/document/preparation.clj
+++ src/orchestration/spike_kubernetes/document/preparation.clj
@@ -1,13 +1,12 @@
 (ns spike-kubernetes.document.preparation
   (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.set :as set]
-            [clojure.string :as str]
             [aid.core :as aid]
             [com.rpl.specter :as s]
             [me.raynes.fs :as fs]
             [spike-kubernetes.command :as command]
-            [spike-kubernetes.helpers :as helpers]
-            [spike-kubernetes.random :as random]))
+            [spike-kubernetes.helpers :as helpers]))
 
 (def downloaded-path
   (helpers/get-dataset-path "downloaded"))
@@ -84,34 +83,29 @@
 (defn organize
   []
   (fs/delete-dir (helpers/get-organized-path))
-  (->>
-    "extracted"
-    helpers/get-dataset-path
-    helpers/get-files
-    random/shuffle
-    (mapcat (comp (partial map (comp (partial s/transform*
-                                              :text
-                                              helpers/structure-document)
-                                     helpers/parse-keywordize))
-                  random/shuffle
-                  str/split-lines
-                  slurp))
-    ((apply juxt
-            (comp (juxt (partial run!
-                                 (aid/build spit-edn-lines+
-                                            (comp helpers/get-training-path
-                                                  (partial (aid/flip str)
-                                                           ".txt")
-                                                  first)
-                                            last))
-                        (comp (partial spit helpers/length-path)
-                              (partial apply hash-map)
-                              (partial mapcat (juxt first
-                                                    (comp count
-                                                          last)))))
-                  (partial map-indexed vector)
-                  (partial map :text)
-                  (partial remove (comp evaluation-ids
-                                        edn/read-string
-                                        :id)))
-            (map make-spit-evaluation [:test :validation])))))
+  (with-open [x (io/reader random-path)]
+    (->> x
+         line-seq
+         (map (comp (partial s/transform*
+                             :text
+                             helpers/structure-document)
+                    helpers/parse-keywordize))
+         ((apply juxt
+                 (comp (juxt (partial run!
+                                      (aid/build spit-edn-lines+
+                                                 (comp helpers/get-training-path
+                                                       (partial (aid/flip str)
+                                                                ".txt")
+                                                       first)
+                                                 last))
+                             (comp (partial spit helpers/length-path)
+                                   (partial apply hash-map)
+                                   (partial mapcat (juxt first
+                                                         (comp count
+                                                               last)))))
+                       (partial map-indexed vector)
+                       (partial map :text)
+                       (partial remove (comp evaluation-ids
+                                             edn/read-string
+                                             :id)))
+                 (map make-spit-evaluation [:test :validation]))))))
