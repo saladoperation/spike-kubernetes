539a91bc4e2a337513a560aa2f3115777f59bcc2
Your Name <you@example.com>
2018-09-30 07:00:48 +0000

    Call tune in -main

--- src/orchestration/spike_kubernetes/core.clj
+++ src/orchestration/spike_kubernetes/core.clj
@@ -4,15 +4,17 @@
             [cats.monad.either :as either]
             [mount.core :as mount]
             [spike-kubernetes.circleci :as circleci]
+            [spike-kubernetes.document.tuning :as tuning]
             [spike-kubernetes.helpers :as helpers]
             [spike-kubernetes.kubernetes :as kubernetes]
-            [spike-kubernetes.preparation :as prepare]))
+            [spike-kubernetes.preparation :as preparation]))
 
 (defn -main
   [command & more]
   (aid/case-eval
     command
-    helpers/preparation-name (prepare/prepare)
+    helpers/preparation-name (preparation/prepare)
+    "tuning" (tuning/tune)
     helpers/orchestration-name (do (require 'spike-kubernetes.orchestration)
                                    (mount/start))
     helpers/kubernetes-name (do (kubernetes/spit-kubernetes)
