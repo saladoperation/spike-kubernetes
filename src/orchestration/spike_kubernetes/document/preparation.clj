cf13bc78c162c41d8b0451f400f35603ab01fc67
Your Name <you@example.com>
2018-10-02 20:19:31 +0000

    Delete an unused dependency

--- src/orchestration/spike_kubernetes/document/preparation.clj
+++ src/orchestration/spike_kubernetes/document/preparation.clj
@@ -3,7 +3,6 @@
             [clojure.java.io :as io]
             [clojure.set :as set]
             [aid.core :as aid]
-            [com.rpl.specter :as s]
             [me.raynes.fs :as fs]
             [spike-kubernetes.command :as command]
             [spike-kubernetes.helpers :as helpers]))
