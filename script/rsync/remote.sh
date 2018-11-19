c6db854a103340204dd0053af023d6a3ff0bdeaa
Your Name <you@example.com>
2018-11-19 21:49:16 +0000

    Delete -r of inotifywait

--- script/rsync/remote.sh
+++ script/rsync/remote.sh
@@ -3,7 +3,7 @@ rsync_selectively(){
     rsync -azP --exclude=.idea --filter=':- /python/.gitignore' $(pwd) $1
 };
 rsync_selectively $1
-while inotifywait -e create -r .git; do
+while inotifywait -e create .git; do
     #.gitignore in the project root seems to be used as a filter by default
     rsync_selectively $1
 done
