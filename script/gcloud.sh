57ff3ada537536a116a3b2c7f59d410a769cc826
Your Name <you@example.com>
2018-11-20 07:02:17 +0000

    Call kubectl apply

--- script/gcloud.sh
+++ script/gcloud.sh
@@ -3,4 +3,5 @@ gcloud auth activate-service-account --key-file=<(echo $GCLOUD_KEY) &&
 gcloud -q config set project $GCLOUD_PROJECT &&
 gcloud -q config set compute/zone $GCLOUD_COMPUTE_ZONE &&
 gcloud container clusters get-credentials $GCLOUD_CLUSTER &&
+kubectl apply -f resources/kubernetes/daemonset-preloaded.yaml &&
 kubectl apply -f resources/kubernetes/kubernetes.txt
