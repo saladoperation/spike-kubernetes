d5d328a8b945f18e973fc5b448d74a14248ffe96
Your Name <you@example.com>
2018-11-21 22:42:54 +0000

    Add a comment about time

--- src/orchestration/spike_kubernetes/kubernetes.clj
+++ src/orchestration/spike_kubernetes/kubernetes.clj
@@ -15,6 +15,21 @@
    :resources {:limits {"nvidia.com/gpu" (aid/case-eval image-name
                                                         helpers/lm-name 1
                                                         0)}}})
+;When grade-lm is run on a GPU:
+;(-> "Jaws is a 1975 American thriller film directed by Steven Spielberg and based on Peter Benchley's 1974 novel of the same name. In the story, a giant man-eating great white shark attacks beachgoers on Amity Island, a fictional New England summer resort town, prompting the local police chief to hunt it with the help of a marine biologist and a professional shark hunter. The film stars Roy Scheider as police chief Martin Brody, Robert Shaw as shark hunter Quint, Richard Dreyfuss as oceanographer Matt Hooper, Murray Hamilton as Larry Vaughn, the mayor of Amity Island, and Lorraine Gary as Brody's wife, Ellen. The screenplay is credited to both Benchley, who wrote the first drafts, and actor-writer Carl Gottlieb, who rewrote the script during principal photography."
+;    helpers/structure-lm
+;    helpers/grade-lm
+;    helpers/generate-lm-inference
+;    helpers/structure-document
+;    helpers/grade-document
+;    helpers/generate-document-inference
+;    count
+;    time)
+;"Elapsed time: 23932.976647 msecs"
+;=> 760
+;When grade-lm is run on 8 CPUs:
+;"Elapsed time: 52743.292594 msecs"
+;=> 760
 
 (def deployment
   {:apiVersion "apps/v1"
