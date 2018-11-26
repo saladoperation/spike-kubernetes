6066daef4ee34f7c05154144b029380ad283c0e3
Your Name <you@example.com>
2018-11-26 04:05:51 +0000

    Add sha256

--- src/orchestration/spike_kubernetes/circleci.clj
+++ src/orchestration/spike_kubernetes/circleci.clj
@@ -66,8 +66,7 @@
 (def node-image
   (str
     node-name
-    ;TODO add the SHA
-    ":8.12.0"))
+    ":8.12.0@sha256:06c7033a274aab131b07e7b8da8d1f823ee752a6e1e2bd46c0b459921ab2c39a"))
 
 (def main-path
   (get-prod-path helpers/main-file))
