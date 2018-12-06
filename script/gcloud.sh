6e9ed52b9bd603195c103311b7a0936baebee545
Your Name <you@example.com>
2018-12-06 03:36:10 +0000

    Add a todo

--- script/gcloud.sh
+++ script/gcloud.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#TODO rewrite this script in Clojure
 gcloud auth activate-service-account --key-file=<(echo $GCLOUD_KEY) &&
 gcloud -q config set project $GCLOUD_PROJECT &&
 gcloud -q config set compute/zone $GCLOUD_COMPUTE_ZONE &&
