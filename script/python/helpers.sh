4ffb79c4def44bade6a74cee06744afeac919cd9
Your Name <you@example.com>
2019-05-27 06:41:07 +0000

    Work around a certificate error

--- script/python/helpers.sh
+++ script/python/helpers.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 cd python
 export PYTHONPATH=$(pwd)
+#https://stackoverflow.com/questions/27835619/urllib-and-ssl-certificate-verify-failed-error
+#PYTHONHTTPSVERIFY works around the following error
+#ssl.SSLError: [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed
+#TODO remove this line
+export PYTHONHTTPSVERIFY=0
 . environment/bin/activate
